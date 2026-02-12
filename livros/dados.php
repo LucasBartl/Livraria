b13eecb68c517151c39a2aae61dfc9061285f67c
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-02-12 09:36:13 -0300

    implementaçao de banco ao projeto

--- livros/dados.php
+++ livros/dados.php
@@ -1,16 +1,31 @@
 <?php 
 
-$livros = [
+
+$db= new PDO('sqlite:database.sqlite');
+$query = $db->query("select * from livros ");
+
+
+$livros = $query -> fetchAll();
+
+/* 
+Anotação do que aconteceu aqui: 
+Antes de implementar o SQLite, nós tínhamos um array livros que tinha todas as informações dos livros 
+cadastrados. Fizemos o seguinte, criamos uma variável db que armazena recebe o banco sqlite, que tem o caminho do banco de dados. 
+Depois criamos uma variavel $query, que faz um select no nosso banco (poderia ser insert, delete e etc). Após isso, criamos uma variável $livros 
+substituindo a array antigo, ou seja agora realmeente estamos usando banco de dados .
+ */
+
+/* $livros = [
     [
         'id' => 1,
         'titulo' => "Senhor dos aneis",
         'autor' => "Tolkien",
         'descricao' => 'Em linguística, a noção de texto é ampla e ainda aberta a uma definição mais precisa. Grosso modo, pode ser entendido como manifestação linguística das ideias de um autor, que serão'
     ],
-    [
+    [  
         'id' => 2,
         'titulo' => "Livro PHP",
-        'autor' => "Pedro",
+        'autor' => "Tjose",
         'descricao' => 'Em linguística, a noção de texto é ampla e ainda aberta a uma definição mais precisa. Grosso modo, pode ser entendido como manifestação linguística das ideias de um autor, que serão'
     ],
     [
@@ -26,7 +41,7 @@ $livros = [
         'descricao' => 'Em linguística, a noção de texto é ampla e ainda aberta a uma definição mais precisa. Grosso modo, pode ser entendido como manifestação linguística das ideias de um autor, que serão'
     ],
 
-];
+]; */
 
 
 ?>
--- livros/index.php
+++ livros/index.php
@@ -1,10 +1,5 @@
 <?php
-echo "Sempre cai no index.php";
-
 require 'functions/functions.php';
-
-
-
 require "routes/routes.php"
 
 
