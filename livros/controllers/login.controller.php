cc94764a366e809058ad2027449d8641a2ff3c4e
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-02-22 10:05:53 -0300

    Colocando criptografia na senha

--- livros/controllers/login.controller.php
+++ livros/controllers/login.controller.php
@@ -22,22 +22,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     //Fazer verificaçao no banco de dados 
     $consultaUsuario = $database->query(
         query: "select * from usuarios 
-             where email = :email    
-                 and senha = :senha ",
+             where email = :email ",
         class: Usuarios::class,
-        params: compact('email', 'senha')
+        params: compact('email')
     )->fetch();
 
-    if($validacao-> naoPassou('login')){
+    if ($validacao->naoPassou('login')) {
         header('location: /login');
         exit();
     }
 
     if ($consultaUsuario) {
-        //Se existir vamos adicionar na sessão que o usuário esta autenticado 
+        if (!password_verify($_POST['senha'], $consultaUsuario->senha)) {
+            flash()->push('validacoes_login', ['Usuário ou senha estão incorretos ! ']);
+            header('location: /login');
+            exit();
+        }
 
+        //Se existir vamos adicionar na sessão que o usuário esta autenticado 
         $_SESSION['auth'] = $consultaUsuario;
-        flash()->push('mensagem', 'seja bem vindo' . $consultaUsuario->nome . '!');
+        flash()->push('mensagem', 'Seja bem vindo ' . $consultaUsuario->nome . '!');
         header("location: /");
         exit();
     }
--- livros/views/livro.view.php
+++ livros/views/livro.view.php
@@ -9,6 +9,49 @@
         </div>
     </div>
     <div class="text-sm mt-2">
-        <?= $livro->descricao?>
+        <?= $livro->descricao ?>
     </div>
+</div>
+
+
+<h2>Avaliações</h2>
+
+<div class=" grid grid-cols-4 gap-4">
+
+    <div class="col-span-3 ">Lista</div>
+
+    <div class="border border-stone-700 rounded  ">
+        <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Login</h1>
+        <form class="px-4 py-3 space-y-4" method="POST">
+            <?php if ($validacoes = flash()->get('validacoes_login')): ?> 
+                <div class="border-red-800 bg-red-900  text-red-400 px-4 py-2 rounded-md border-2">
+                    <ul>
+                        <li>Algo esta errado </li>
+                        <?php foreach ($validacoes as $validacao): ?>
+                            <li><?= $validacao ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+            <div class="flex flex-col">
+                <label class="text-stone-400  mb-1">Email</label>
+                <input type="email"
+                    class="border-stone-800 border-2 items-center rounded-md bg-stone-900 text-sm focus:outline-none px-2 py1"
+                    placeholder=""
+                    name="email">
+            </div>
+            <div class="flex flex-col">
+                <label class="text-stone-400  mb-1">Senha</label>
+                <input type="password"
+                    class="border-stone-800 border-2 items-center rounded-md bg-stone-900 text-sm focus:outline-none px-2 py1"
+                    placeholder=""
+                    name="senha">
+            </div>
+
+            <button type="submit" class="border-stone-800 bg-stone-900  text-stone-400 px-4 py-2 rounded-md border-2 hover:bg-stone-700">Logar</button>
+        </form>
+    </div>
+
+
+
 </div>
